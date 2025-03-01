0df174678f94b2b6abeadd3777153f0c0a33f66f
Abdallah Yassein <abdallah.work.1998@gmail.com>
2025-03-01 13:38:19 +0200

    added custom button

---
+++ lib/cutom_primary_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomPrimayButton extends StatelessWidget {
+  const CustomPrimayButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
