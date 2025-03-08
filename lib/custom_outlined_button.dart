0f5269e22db1746997ed49dc04934ebe8b34d531
Abdallah Yassein <abdallah.work.1998@gmail.com>
2025-03-08 14:24:37 +0200

    added outlied button

--- lib/custom_outlined_button.dart
+++ lib/custom_outlined_button.dart
@@ -10,6 +10,22 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+        foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+        overlayColor: WidgetStateProperty.all<Color>(Colors.green),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text('Custom Outlined Button'),
+      ),
+    );
   }
 }
